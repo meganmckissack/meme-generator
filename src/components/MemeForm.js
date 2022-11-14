cfe6d4d532dfa09e2abb304e041b401010e8cdec
Megan McKissack <meganmckissack@gmail.com>
2022-11-13 17:00:04 -0800

    update to include state and dummy api

--- src/components/MemeForm.js
+++ src/components/MemeForm.js
@@ -1,10 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import MemeData from '../memeData.js'
 
 function MemeForm() {
+  const [memeImgVisible, setMemeImgVisible] = useState(null);
+
+  const handleClick = () => {
+    const memesArray = MemeData.data.memes;
+    const randomNumber = Math.floor(Math.random() * memesArray.length);
+    const url = memesArray[randomNumber].url;
+    setMemeImgVisible(url);
+    console.log(url);
+    // var imgResults = [];
+    // for(const image in memesArray) {
+    //   if(memesArray.hasOwnProperty(image)) {
+    //     imgResults.push(image);
+    //   }
+    // }
+    // return imgResults[imgResults.length * Math.random() << 0];
+  }
+
+
   return(
     <React.Fragment>
       <main>
-        <form className="form">
+        <div className="form">
           <input className="form-input"
             type='text' 
             name='name'
@@ -13,8 +32,9 @@ function MemeForm() {
             type='text' 
             name='name'
             placeholder="text" />
-          <button type='submit' className="form-button">Get Meme</button>
-        </form>
+          <button className="form-button" onClick={handleClick}>Get Meme</button>
+        </div>
+        <img src={memeImgVisible} className="meme-image" />
       </main>
     </React.Fragment>
   )
